06041baf0aa11f01f5fd855edd2fd7e99ae3769a
Menzi-B <menzi.bomba@gmail.com>
2024-12-23 13:43:43 +0200

    Added input validation logic to ensure all input is of type numeric

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 function NPV(effectiveRate,cashFlow)
 {
     try{
+
+        if(typeof effectiveRate === 'string' || typeof cashFlow === 'string')
+            //console.log("Input validation error occurred: ", "please ensure all input value is/are of type numeric");
+            throw new Error("\x1b[31m Input validation error - please ensure all input value is/are of type numeric \x1b[0m");
+        else
+            1
+
         let sum = 0;
-        //const discountedCashFlows = cashFlow.map(n => n/(1+effectiveRate));
-        //const sum = discountedCashFlows.reduce((total, n) => total + n, 0);
         for(var i = 0; i < cashFlow.length; i++){
             console.log(cashFlow[i]/(1+effectiveRate)**(i+1));
             sum += cashFlow[i]/(1+effectiveRate)**(i+1);
